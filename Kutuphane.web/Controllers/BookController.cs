5e72b11b81557e81958b9afcd282c7967550e5d3
brspsn <brspesen@gmail.com>
2023-08-29 17:09:36 +0300

    PublisherController has been added. Work continues on the book controller.

---
+++ Kutuphane.web/Controllers/BookController.cs
@@ -0,0 +1,32 @@
+﻿using Kutuphane.Data;
+using Kutuphane.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kutuphane.web.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BookController(ApplicationDbContext context) 
+        {
+            _context=context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Books.ToList<Book>());
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add(Book book)
+        {
+            _context.Books.Add(book);   
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- Kutuphane.web/Controllers/PublisherController.cs
+++ Kutuphane.web/Controllers/PublisherController.cs
@@ -4,49 +4,52 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Kutuphane.web.Controllers
 {
-    public class AuthorController:Controller
+    public class PublisherController:Controller
     {
         private readonly ApplicationDbContext _context;
-        public AuthorController(ApplicationDbContext context)
+        
+        public PublisherController(ApplicationDbContext context)
         {
-            _context = context;
+            _context=context;
         }
+        
         public IActionResult Index()
         {
-            return View(_context.Author.ToList<Author>());
+            return View(_context.Publisher.ToList<Publisher>());
         }
-        [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
-        [HttpPost]
-        public IActionResult Add(Author author)
+        [HttpGet]
+        public IActionResult Add( Publisher publisher)
         {
-            _context.Author.Add(author);
+            _context.Publisher.Add(publisher);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View();
+        }
         public IActionResult Delete(int id)
         {
-            Author authorTobeDeleted = _context.Author.Find(id);
-            _context.Author.Remove(authorTobeDeleted);
+            Publisher PublisherTobeDeleted = _context.Publisher.Find(id);
+            _context.Publisher.Remove(PublisherTobeDeleted);
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
         public IActionResult Edit(int id)
         {
-            return View(_context.Author.Find(id));
-
+            return View(_context.Publisher.Find(id));
         }
         [HttpPost]
-        public IActionResult Edit(Author author)
+        public IActionResult Edit(Publisher Publisher)
         {
-            _context.Author.Update(author);
+            _context.Publisher.Update(Publisher);
             _context.SaveChanges();
             return RedirectToAction("Index");
-
         }
-
     }
 }
